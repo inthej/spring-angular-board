616a9159f7b0fb551e81c51603e1a165fe6a94c1
inthej <inthej12@gmail.com>
2023-10-21 19:25:02 +0900

    feat: 레이아웃 기반 라우팅 설정

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from "@angular/router";
+import { MainLayoutComponent } from "./layouts/main-layout/main-layout.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { BoardListComponent } from "./pages/board/board-list/board-list.component";
+import { BoardViewComponent } from "./pages/board/board-view/board-view.component";
+import { EmptyLayoutComponent } from "./layouts/empty-layout/empty-layout.component";
+import { LoginComponent } from "./pages/login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'board/list', component: BoardListComponent },
+      { path: 'board/view/:id', component: BoardViewComponent },
+      { path: 'board/:mode', component: BoardViewComponent },
+    ]
+  },
+  {
+    path: 'login',
+    component: EmptyLayoutComponent,
+    children: [
+      { path: '', component: LoginComponent },
+    ]
+  },
+  { path: 'board', redirectTo: 'board/list', pathMatch: 'full' },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmptyLayoutComponent } from './layouts/empty-layout/empty-layout.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BoardViewComponent } from './pages/board/board-view/board-view.component';
+import { BoardListComponent } from './pages/board/board-list/board-list.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmptyLayoutComponent,
+    MainLayoutComponent,
+    LoginComponent,
+    HomeComponent,
+    BoardViewComponent,
+    BoardListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}

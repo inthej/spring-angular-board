13d5c057c0ac5faaeab96f58ebe50e1eb4b70c66
inthej <inthej12@gmail.com>
2023-10-29 16:55:59 +0900

    update: title & meta description 처리

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  constructor(private title: Title, private meta: Meta) {
+    this.setTitle('Spring-Angular-Board');
+    this.setMetaDescription('스프링 앵귤러 게시판');
+  }
+
+  private setTitle(newTitle: string): void {
+    this.title.setTitle(newTitle);
+  }
+
+  private setMetaDescription(description: string): void {
+    this.meta.updateTag({ name: 'description',  content: description})
+  }
 }
